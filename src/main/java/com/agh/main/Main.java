1dc0b38b8a314f652a75ecbef672f24b04e38b56
mateuszzawadzki <mateuszzawadzki555@gmail.com>
2018-04-24 23:32:43 +0200

    main refactor

--- src/main/java/com/agh/main/Main.java
+++ src/main/java/com/agh/main/Main.java
@@ -23,7 +23,7 @@ public class Main {
     }
 
     private static void initializeProperties(String[] args) throws Exception {
-        Files.walk(Paths.get("src\\main\\resources\\configs"))
+        Files.walk(Paths.get(args[0]))
             .filter(Files::isRegularFile)
             .map(Path::toFile)
             .forEach(file -> {
--- src/main/java/com/agh/properties/PropertiesUtils.java
+++ src/main/java/com/agh/properties/PropertiesUtils.java
@@ -9,7 +9,7 @@ import java.util.Properties;
 public class PropertiesUtils {
 
     public static final String INTERVAL = "interval";
-    public static final String PROTOCOL = "com/agh/protocol";
+    public static final String PROTOCOL = "protocol";
 
     public static boolean validateProperties(Properties properties) {
         return validateProtocol(properties) && validateInterval(properties);
