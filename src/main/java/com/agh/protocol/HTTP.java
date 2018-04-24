d67df36c924f849104449404ac0f3959370b6c2e
mateuszzawadzki <mateuszzawadzki555@gmail.com>
2018-04-25 00:25:16 +0200

    logs

--- src/main/java/com/agh/protocol/HTTP.java
+++ src/main/java/com/agh/protocol/HTTP.java
@@ -6,8 +6,13 @@ import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HTTP extends ConnectionProtocol {
+
+    private static final DateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
 
     @Override
     public void downloadLogs() {
@@ -15,7 +20,10 @@ public class HTTP extends ConnectionProtocol {
         System.out.println("Downloading file from " + url);
         try {
             try (InputStream in = URI.create(url).toURL().openStream()) {
-                Files.copy(in, Paths.get(properties.getProperty("saveTo")));
+                String date = df.format(new Date());
+                String filename = properties.getProperty("saveTo") + date;
+                System.out.println("Saving to file: " + filename);
+                Files.copy(in, Paths.get(filename));
             }
         } catch (IOException e) {
             e.printStackTrace();
